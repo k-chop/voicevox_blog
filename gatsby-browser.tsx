73bceccdaaf405b64159e247fb5ff11bcc40804c
k-chop <whelmaze@gmail.com>
2023-01-22 12:46:09 +0900

    ページ間遷移アニメーションを実現するためAnimationPresenceで全体をwrap

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -5,6 +5,13 @@
  */
 
 // You can delete this file if you're not using it
+import type { GatsbyBrowser } from "gatsby"
 import { config } from "@fortawesome/fontawesome-svg-core"
 import "@fortawesome/fontawesome-svg-core/styles.css"
+import React from "react"
+import { AnimatePresence } from "framer-motion"
 config.autoAddCss = false
+
+export const wrapPageElement: GatsbyBrowser["wrapPageElement"] = ({
+  element,
+}) => <AnimatePresence mode="wait">{element}</AnimatePresence>
